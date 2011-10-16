e795c3864721af7674274902876981c32dd2ffce
Arjun <arjunbatra03@gmail.com>
2011-10-15 22:07:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PinkElephant::Application.config.secret_token = 'c27692ab0661f4abcf4ac2fbc3a6c7bef7cce310cfd9705498686bbac0c3cce3a52cb2bb1a01404d2f49dd7a7c5451f49f776faa4ba02589ed176dcca56b5ca1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PinkElephant::Application.config.session_store :cookie_store, :key => '_pink_elephant_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# PinkElephant::Application.config.session_store :active_record_store
